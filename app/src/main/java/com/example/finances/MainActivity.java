fc492d6941e07c3dbd5411c2c097857c5f534994
pas-comme <imahalimanana@gmail.com>
2025-07-30 21:16:20 +0300

    init

---
+++ app/src/main/java/com/example/finances/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.finances;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.VIBRATE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.ScanMode;
+
+import org.json.JSONException;
+
+public class MainActivity extends AppCompatActivity {
+    //widget
+    ViewFlipper viewFlipper;
+    RequestQueue requestQueue;
+    CodeScannerView scanView;
+    CodeScanner scanner;
+    Button valider;
+    TextView validerTV;
+    EditText mdp, confirmation;
+
+    // variable et constante
+    String id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        valider = findViewById(R.id.validerBTN);
+        viewFlipper = findViewById(R.id.financeVF);
+        requestQueue = Volley.newRequestQueue(this);
+        validerTV = findViewById(R.id.validerTV);
+        mdp = findViewById(R.id.mdp);
+        confirmation = findViewById(R.id.confirmation);
+
+        if(!checkPermission()) {
+            Toast.makeText(this, "Vous devez accepter les permissions requise pour utiliser l'application", Toast.LENGTH_LONG).show();
+            requestPermisssion();
+        }
+        else{
+            scanView = findViewById(R.id.camVIEW);
+            scanner = new CodeScanner(this, scanView);
+            scanner.setCamera(CodeScanner.CAMERA_BACK);
+            scanner.setFormats(CodeScanner.ALL_FORMATS);
+            scanner.setAutoFocusMode(AutoFocusMode.SAFE);
+            scanner.setScanMode(ScanMode.CONTINUOUS);
+            scanner.setAutoFocusEnabled(true);
+            scanner.setFlashEnabled(false);
+
+            scanner.setDecodeCallback(result -> runOnUiThread(() -> {
+                scanner.stopPreview();
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(1000);
+                id = result.getText();
+                String url = "http://10.0.49.123:5000/API/citizens/exists?id=" + id;
+                @SuppressLint("SetTextI18n") JsonObjectRequest requesti = new JsonObjectRequest(Request.Method.GET, url, null,
+                        reponse -> {
+                            String ter = "";
+                            try {
+                                ter = reponse.getString("reponse");
+                            } catch (JSONException e) {
+                                Log.d("erreur", e.toString());
+                            }
+                            if (ter.equals("cet utilisateur n'existe plus")){
+                                Toast.makeText(this, ter, Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                viewFlipper.showNext();
+                                String urlm = "http://10.0.49.169:5000/API/finances/login?id=" + id;
+                                @SuppressLint("SetTextI18n") JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urlm, null,
+                                        response -> {
+                                            boolean bool = false;
+                                            try {
+                                                bool = response.getInt("id") == 0;
+                                            } catch (JSONException e) {
+                                                Log.d("erreur", e.toString());
+                                            }
+                                            if (bool){
+                                                validerTV.setText("Vous n'êtes pas encore de compte pour en créer ajouter un mot de passe puis resaisissez le pour le confirmer");
+                                                confirmation.setVisibility(View.VISIBLE);
+                                                valider.setOnClickListener(view -> {
+                                                    if (mdp.getText().toString().isEmpty())
+                                                        Toast.makeText(this, "ERREUR : mot de passe vide", Toast.LENGTH_LONG).show();
+                                                    else {
+                                                        if (mdp.getText().toString().equals(confirmation.getText().toString())){
+                                                            String urli = "http://10.0.49.169:5000/API/finances/enrolment?id=" + id + "&mdp=" + mdp.getText().toString().hashCode();
+                                                            StringRequest requestI = new StringRequest(Request.Method.GET, urli,
+                                                                    response1 -> {
+                                                                        Toast.makeText(this, response1, Toast.LENGTH_LONG).show();
+                                                                        Intent secondeActivite = new Intent(MainActivity.this,
+                                                                                MainActivity2.class);
+                                                                        secondeActivite.putExtra("id", id);
+                                                                        secondeActivite.putExtra("mdp", mdp.getText().toString().hashCode());
+                                                                        startActivity(secondeActivite);
+                                                                        finish();
+                                                                    },
+                                                                    error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+                                                            requestQueue.add(requestI);
+                                                        }
+                                                        else
+                                                            Toast.makeText(this, "ERREUR : mot de passe non confirmé", Toast.LENGTH_LONG).show();
+                                                    }
+
+                                                });
+                                            }
+                                            else {
+                                                valider.setOnClickListener(view -> {
+                                                    if (mdp.getText().toString().isEmpty())
+                                                        Toast.makeText(this, "ERREUR : mot de passe vide", Toast.LENGTH_LONG).show();
+                                                    else {
+                                                        try {
+                                                            if (mdp.getText().toString().hashCode() == response.getInt("mdp")){
+                                                                Toast.makeText(this, "identifiant confirmé", Toast.LENGTH_LONG).show();
+                                                                Intent secondeActivite = new Intent(MainActivity.this,
+                                                                        MainActivity2.class);
+                                                                secondeActivite.putExtra("id", id);
+                                                                secondeActivite.putExtra("mdp", mdp.getText().toString().hashCode());
+                                                                secondeActivite.putExtra("solde", response.getDouble("solde"));
+                                                                startActivity(secondeActivite);
+                                                                finish();
+                                                            }
+                                                        } catch (JSONException e) {
+                                                            Log.d("erreur", e.toString());
+                                                        }
+                                                    }
+                                                });
+                                            }
+                                        },
+                                        error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+                                requestQueue.add(request);
+                            }
+                        },
+                        error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+                requestQueue.add(requesti);
+            }));
+            scanner.setErrorCallback(error -> runOnUiThread(() ->
+                    Toast.makeText(this, error.getMessage(), Toast.LENGTH_LONG).show()));
+            scanner.startPreview();
+        }
+
+    }
+
+    private boolean checkPermission(){
+        int read = ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE);
+        int cam_permission = ContextCompat.checkSelfPermission(this,CAMERA);
+        int storage_permission = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
+        int vibration_permission = ContextCompat.checkSelfPermission(this, VIBRATE);
+        return cam_permission == PackageManager.PERMISSION_GRANTED && storage_permission == PackageManager.PERMISSION_GRANTED &&
+                vibration_permission == PackageManager.PERMISSION_GRANTED && read == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestPermisssion(){
+        final int PERMISSION_CODE = 200;
+        ActivityCompat.requestPermissions(this, new String[]{
+                CAMERA, VIBRATE, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+    }
+}
---
+++ app/src/main/java/com/example/finances/MainActivity2.java
@@ -0,0 +1,258 @@
+package com.example.finances;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.ScanMode;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
+
+public class MainActivity2 extends AppCompatActivity {
+    //widget
+    ViewFlipper viewFlipper;
+    RequestQueue requestQueue;
+    Button depot, retrait, soldeBTN, trasaction, retourSolde, retourTransaction;
+    TextView soldeTV;
+    ListView list;
+
+    //variable et constante
+    String id, idCP;
+    int mdp, mdpCP;
+    Double solde;
+    ArrayList<JSONObject> array = new ArrayList<>();
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        viewFlipper = findViewById(R.id.acceuilVF);
+        list = findViewById(R.id.liste);
+        depot = findViewById(R.id.depotBTN);
+        retrait = findViewById(R.id.retraitBTN);
+        soldeBTN = findViewById(R.id.consultationBTN);
+        trasaction = findViewById(R.id.transactionBTN);
+        soldeTV = findViewById(R.id.soldeTV);
+        retourSolde = findViewById(R.id.retourSolde);
+        retourTransaction = findViewById(R.id.retourTransaction);
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        Intent i = getIntent();
+        id = i.getStringExtra("id");
+        solde = i.getDoubleExtra("solde", 0);
+        mdp = i.getIntExtra("mdp", 0);
+
+        String urlp = "http://10.0.49.123:5000/API/citizens/onePRS?id=" + id;
+        JsonObjectRequest requestP = new JsonObjectRequest(Request.Method.GET, urlp, null,
+                response -> {
+
+                    try {
+
+                        String [] temp = response.getString("daty").split("-");
+                        Date actu = new Date();
+                        @SuppressLint("SimpleDateFormat") DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                        String [] vao = format.format(actu).split("-");
+                        int age = Integer.parseInt(vao[2]) - Integer.parseInt(temp[2]);
+                        this.setTitle(response.getString("anarana") + " " +
+                                response.getString("fanampiny") + " " + age + " ans "+ response.getString("sexe"));
+
+                    } catch (JSONException e) {
+                        Log.d("erreur", e.toString());
+                    }
+
+                },
+                error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+        requestQueue.add(requestP);
+
+
+        retourSolde.setOnClickListener(view -> viewFlipper.showPrevious());
+        retourTransaction.setOnClickListener(view -> {
+            viewFlipper.showPrevious();viewFlipper.showPrevious();
+            array.clear();
+        });
+        retrait.setOnClickListener(view -> dialogue("retrait"));
+        depot.setOnClickListener(view -> dialogue("dépôt"));
+
+        soldeBTN.setOnClickListener(view -> {
+            viewFlipper.showNext();
+            soldeTV.setText("Votre solde actuel est de  : " + solde);
+        });
+
+        trasaction.setOnClickListener(view -> {
+            String url = "http://10.0.49.169:5000/API/finances/deals?id=" + id;
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                    response -> {
+                        try {
+                            for (int j = 0; j < response.length(); j++) {
+                                array.add((JSONObject) response.get(j));
+                            }
+                        } catch (JSONException e) {
+                            Log.d("erreur", e.toString());
+                        }
+
+                        list.setAdapter(new JSONadapter(this, R.layout.help, array));
+                        viewFlipper.showNext();viewFlipper.showNext();
+                    },
+                    error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+            requestQueue.add(request);
+        });
+    }
+    @SuppressLint("SetTextI18n")
+    public void dialogue(String action){
+        Dialog dlg = new Dialog(this);
+        @SuppressLint("InflateParams")
+        View alertView = LayoutInflater.from(this).inflate(R.layout.item, null);
+        dlg.setContentView(alertView);
+        TextView titre = alertView.findViewById(R.id.titreDLG);
+        if (action.equals("dépôt"))
+            titre.setText("Saisissez le montant à déposer suivi de votre mot de passe");
+        else if(action.equals("retrait"))
+            titre.setText("Saisissez le montant à retirer suivi de votre mot de passe");
+
+        EditText montant = alertView.findViewById(R.id.montant),
+                pwd = alertView.findViewById(R.id.mdp), pwdCP = alertView.findViewById(R.id.mdpCP);
+        Button valider = alertView.findViewById(R.id.valider), suivant = alertView.findViewById(R.id.suivant);
+        suivant.setOnClickListener(view -> {
+            ViewFlipper vf = alertView.findViewById(R.id.vfDLG);
+            vf.showNext();
+            CodeScannerView scanView = alertView.findViewById(R.id.camVIEW);
+            CodeScanner scanner = new CodeScanner(this, scanView);
+            scanner.setCamera(CodeScanner.CAMERA_BACK);
+            scanner.setFormats(CodeScanner.ALL_FORMATS);
+            scanner.setAutoFocusMode(AutoFocusMode.SAFE);
+            scanner.setScanMode(ScanMode.CONTINUOUS);
+            scanner.setAutoFocusEnabled(true);
+            scanner.setFlashEnabled(false);
+            scanner.setDecodeCallback(result -> runOnUiThread(() -> {
+                scanner.stopPreview();
+                String url = "http://10.0.49.123:5000/API/citizens/exists?id=" + id;
+                @SuppressLint("SetTextI18n") JsonObjectRequest requesti = new JsonObjectRequest(Request.Method.GET, url, null,
+                        reponse -> {
+                            vf.showPrevious();
+                            valider.setVisibility(View.VISIBLE);
+                            idCP = result.getText();
+                            pwdCP.setVisibility(View.VISIBLE);
+                            suivant.setVisibility(View.GONE);
+
+                            String urlm = "http://10.0.49.169:5000/API/finances/login?id=" + result.getText();
+                            @SuppressLint("SetTextI18n") JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urlm, null,
+                                    response -> {
+                                        try {
+                                            mdpCP = response.getInt("mdp");
+                                        } catch (JSONException e) {
+                                            Log.d("erreur", e.toString());
+                                        }
+                                    },
+                                    error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+                            requestQueue.add(request);
+                        },
+                        error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+                requestQueue.add(requesti);
+
+            }));
+            scanner.setErrorCallback(error -> runOnUiThread(() ->
+                    Toast.makeText(this, error.getMessage(), Toast.LENGTH_LONG).show()));
+            scanner.startPreview();
+        });
+        valider.setOnClickListener(view -> {
+            if (montant.getText().toString().isEmpty() || pwd.getText().toString().isEmpty())
+                Toast.makeText(this, "ERREUR : vous devez remplir tous les champs", Toast.LENGTH_LONG).show();
+            else if (volaIScorrect(montant.getText().toString()))
+                Toast.makeText(this, "ERREUR : montant invalide", Toast.LENGTH_LONG).show();
+            else if (action.equals("retrait") && Double.parseDouble(montant.getText().toString()) > solde)
+                Toast.makeText(this, "ERREUR : montant de retrait supérieur au solde", Toast.LENGTH_LONG).show();
+            else if (pwd.getText().toString().hashCode() != mdp)
+                Toast.makeText(this, "ERREUR : mot de passe client incorrecte", Toast.LENGTH_LONG).show();
+            else if (pwdCP.getText().toString().hashCode() != mdpCP)
+                Toast.makeText(this, "ERREUR : mot de passe cash point incorrecte", Toast.LENGTH_LONG).show();
+            else{
+                if (action.equals("retrait"))
+                    solde -= Double.parseDouble(montant.getText().toString());
+                else if (action.equals("dépôt"))
+                    solde += Double.parseDouble(montant.getText().toString());
+                @SuppressLint("SimpleDateFormat") DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                String temp = format.format(new Date());
+
+                String urli = "http://10.0.49.169:5000/API/finances/changeBalance?solde=" + solde + "&id=" + id
+                        + "&type=" + action + "&montant=" + montant.getText() + "&idCP=" + idCP + "&daty=" + temp;
+                StringRequest requestI = new StringRequest(Request.Method.GET, urli,
+                        response1 -> {
+                            Toast.makeText(this, response1, Toast.LENGTH_LONG).show();
+                            dlg.dismiss();
+                        },
+                        error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show());
+                requestQueue.add(requestI);
+            }
+        });
+        dlg.show();
+    }
+    public static class JSONadapter extends ArrayAdapter<JSONObject> {
+        private final Context context;
+        private final int mResource;
+        public JSONadapter(@NonNull Context context, int resource, ArrayList<JSONObject> objects) {
+            super(context, resource, objects);
+            this.context = context;
+            this.mResource = resource;
+        }
+        @NonNull
+        @SuppressLint({"ViewHolder", "SetTextI18n"})
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView = layoutInflater.inflate(mResource, parent, false);
+
+            TextView daty = convertView.findViewById(R.id.date);
+            TextView description = convertView.findViewById(R.id.description);
+
+            try {
+
+                daty.setText(getItem(position).optString("daty"));
+                description.setText(
+                        getItem(position).getString("type") + " de " + Objects.requireNonNull(getItem(position)).getString("montant"));
+
+            } catch (JSONException e) {
+                Log.d("erreur", e.toString());
+            }
+            return convertView;
+        }
+
+    }
+    public boolean volaIScorrect(String vola){
+        return !vola.matches("[1-9][0-9.]*");
+    }
+}
